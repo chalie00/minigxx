95137e366acd3a835f5201d2b7cc39ef0f391aa2
chalie <formathead79@gmail.com>
2024-03-20 15:34:11 +0900

    Revert "20240320"
    This reverts commit fd4cfd99251c5248fc92871facd1954bc764d88c.

--- Gimbal_mini/Core/Src/main.c
+++ Gimbal_mini/Core/Src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "stdio.h"
-#include "stm32f100xb.h"
 
 /* USER CODE END Includes */
 
@@ -465,37 +464,32 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
     uint8_t i;
     if (huart->Instance == USART2) {
         uart2_rx_buffer[uart2_rx_index] = USART2->DR;
-        if (uart2_rx_buffer[uart2_rx_index] == 0xAA) {
-            if (uart2_rx_index + 1 >= UART2_BUFFER_SIZE) {
-                if (uart2_rx_buffer[0] == 0xAA && uart2_rx_buffer[1] == 0x05) {
-                    for (i = 0; i < 6; i++) {
-                        checksum += uart2_rx_buffer[i];
-                    }
-                    if (checksum == uart2_rx_buffer[6]) {
-                        HAL_UART_Transmit(&huart4, (uint8_t *) uart2_rx_buffer, 9, 1);
-                        uart2_rx_index = 0;
-                    } else {
-                        for (i = 0; i < 10; i++) {
-                            uart2_rx_buffer[i] = 0x00;
-                            uart2_rx_index = 0;
-                        }
-                    }
+        if (uart2_rx_index + 1 == UART2_BUFFER_SIZE) {
+            if (uart2_rx_buffer[0] == 0xAA && uart2_rx_buffer[1] == 0x05) {
+                for (i = 0; i < 6; i++) {
+                    checksum += uart2_rx_buffer[i];
+                }
+                if (checksum == uart2_rx_buffer[6]) {
+                    HAL_UART_Transmit(&huart4, (uint8_t *) uart2_rx_buffer, 9, 1);
+                    uart2_rx_index = 0;
                 } else {
-                    for (i = 0; i < 10; i++) {
+                    for (i = 0; i < 9; i++) {
                         uart2_rx_buffer[i] = 0x00;
                         uart2_rx_index = 0;
                     }
                 }
             } else {
-                uart2_rx_index++;
+                for (i = 0; i < 9; i++) {
+                    uart2_rx_buffer[i] = 0x00;
+                    uart2_rx_index = 0;
+                }
             }
         } else {
-            //TODO: if not IR CMD, check whether it's other device CMD
-            uart2_rx_buffer[uart2_rx_index] = 0x00;
-            uart2_rx_index = 0;
+            uart2_rx_index++;
         }
+//        HAL_UART_Transmit(&huart4, (uint8_t *) uart2_rx_buffer, 1, 8);
     }
-    HAL_UART_Receive_IT(&huart2, (uint8_t *) uart2_rx_buffer, 1);
+    HAL_UART_Receive_IT(&huart2, (uint8_t * ) uart2_rx_buffer, 1);
 }
 /* USER CODE END 4 */
 
